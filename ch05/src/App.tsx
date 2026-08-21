2501d131209b8a7d9e566e3b2ff5002640f8f4aa
harperppppppp <harperp.bz@gmail.com>
2026-08-21 11:24:41 +0900

    0820

--- ch05/src/App.tsx
+++ ch05/src/App.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Child from './components/Child'
+import Child2 from './components/Child2'
+import Child3 from './components/Child3'
+import StyledChild from './components/StyledChild'
+import StyledChild2 from './components/StyledChild2'
+import Emotion from './components/Emotion'
+import love from './assets/images/양갱이.jpg'
 
 function App() {
   const styles = {
@@ -16,6 +22,23 @@ function App() {
       <h1 style={styles}>Inline Style</h1>
       <button className='btn'>app button</button>
       <Child />
+      <Child2 />
+      <Child3 />
+      <hr/>
+      <StyledChild />
+      <StyledChild2 />
+      <Emotion />
+      <hr />
+      <div>
+        <button className ='bg-transparent'>테일윈드</button>
+      </div>
+      <img src={love} alt='내사랑' className='img600'/>
+      <div className='img2'></div>
+      <div style={{width:'300px', height:'300px', 
+        background: `url(${love}) center/cover`}}></div>
+        <p>
+          양갱이귀여워장모치와와인줄알았는데잡종이었던양갱이귀여워
+        </p>
     </>
   )
 }
---
+++ ch05/src/components/Child3.tsx
@@ -0,0 +1,14 @@
+import classNames from "classnames";
+import { useState } from "react";
+
+export default function Child3(){
+  const [isActive,setIsActive] = useState(false);
+  const handleClick = ()=>{
+    setIsActive((prev)=>!prev);
+  }
+  return(
+    <>
+    <button className={classNames({btn:true, 'is-active':isActive})} onClick = {handleClick}>App Button</button>
+    </>
+  )
+}

b79fc036cc90d63776b054ff5d36f7714e4918f6
harperppppppp <harperp.bz@gmail.com>
2026-08-28 12:28:11 +0900

    0828_ch09

--- ch09/src/App.tsx
+++ ch09/src/App.tsx
@@ -1,120 +1,121 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
+      <div className="todo">
+        <h1 className="todo__title">Todo List</h1>
+        <p className="todo__subtitle">Please enter your details to continue.</p>
+        {/* <!-- 할 일 등록 --> */}
+        <form className="todo__form">
+          <div className="todo__editor">
+            <input
+              type="text"
+              className="todo__input"
+              placeholder="Enter Todo List"
+            />
+            <button className="todo__button" type="submit">Add</button>
+          </div>
+        </form>
+        {/* <!-- 할 일 목록 --> */}
+        <ul className="todo__list">
+          {/* <!-- 할 일 목록이 없을 때 --> */}
+          <li className="todo__item todo__item--empty">
+            <p className="todo__text--empty">There are no registered tasks</p>
+          </li>
+          {/* <!-- 할 일 목록이 있을 때 --> */}
+          {/* <!-- 할 일이 완료되면 .todo__item--complete 추가 --> */}
+          <li className="todo__item todo__item--complete">
+            <div className="todo__checkbox-group">
+              <input type="checkbox" className="todo__checkbox" checked />
+              <label>Eat Breakfast</label>
+            </div>
+            {/* <!-- 할 일을 수정할 때만 노출 (.todo__checkbox-group은 비노출) --> */}
+            {/* <!-- <input type="text" className="todo__modify-input" /> --> */}
+            <div className="todo__button-group">
+              <button className="todo__action-button">
                 <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
+                  width="23"
+                  height="23"
+                  viewBox="0 0 23 23"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <use href="/icons.svg#github-icon"></use>
+                  <path
+                    d="M12.45 7.04937L15.9505 10.55L8.34938 18.1514L5.22844 18.4959C4.81064 18.5421 4.45764 18.1888 4.50412 17.771L4.85138 14.6478L12.45 7.04937ZM18.1155 6.52819L16.4719 4.88453C15.9592 4.37182 15.1277 4.37182 14.615 4.88453L13.0688 6.43084L16.5692 9.93145L18.1155 8.38513C18.6282 7.87215 18.6282 7.04089 18.1155 6.52819Z"
+                    fill="#4F4F4F"
+                  />
                 </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
+              </button>
+              <button className="todo__action-button">
                 <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
+                  width="15"
+                  height="16"
+                  viewBox="0 0 15 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <use href="/icons.svg#discord-icon"></use>
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M7.50002 9.81827L12.9548 15.2731L14.7731 13.4548L9.31829 8L14.7731 2.54518L12.9548 0.726901L7.50002 6.18173L2.04519 0.726902L0.226918 2.54518L5.68174 8L0.226919 13.4548L2.04519 15.2731L7.50002 9.81827ZM7.50002 9.81827L9.31829 8L7.50002 6.18173L5.68174 8L7.50002 9.81827Z"
+                    fill="#4F4F4F"
+                  />
+                  <path
+                    d="M7.50002 9.81827L9.31829 8L7.50002 6.18173L5.68174 8L7.50002 9.81827Z"
+                    fill="#4F4F4F"
+                  />
                 </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
+              </button>
+            </div>
+          </li>
+          <li className="todo__item todo__item--complete">
+            {/* <!-- <div className="todo__checkbox-group">
+              <input type="checkbox" className="todo__checkbox" checked />
+              <label>Eat Breakfast</label>
+            </div> --> */}
+            {/* <!-- 할 일을 수정할 때만 노출 (.todo__checkbox-group은 비노출) --> */}
+            <input type="text" className="todo__modify-input" />
+            <div className="todo__button-group">
+              <button className="todo__action-button">
                 <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
+                  width="23"
+                  height="23"
+                  viewBox="0 0 23 23"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <use href="/icons.svg#x-icon"></use>
+                  <path
+                    d="M12.45 7.04937L15.9505 10.55L8.34938 18.1514L5.22844 18.4959C4.81064 18.5421 4.45764 18.1888 4.50412 17.771L4.85138 14.6478L12.45 7.04937ZM18.1155 6.52819L16.4719 4.88453C15.9592 4.37182 15.1277 4.37182 14.615 4.88453L13.0688 6.43084L16.5692 9.93145L18.1155 8.38513C18.6282 7.87215 18.6282 7.04089 18.1155 6.52819Z"
+                    fill="#4F4F4F"
+                  />
                 </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+              </button>
+              <button className="todo__action-button">
                 <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
+                  width="15"
+                  height="16"
+                  viewBox="0 0 15 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <use href="/icons.svg#bluesky-icon"></use>
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M7.50002 9.81827L12.9548 15.2731L14.7731 13.4548L9.31829 8L14.7731 2.54518L12.9548 0.726901L7.50002 6.18173L2.04519 0.726902L0.226918 2.54518L5.68174 8L0.226919 13.4548L2.04519 15.2731L7.50002 9.81827ZM7.50002 9.81827L9.31829 8L7.50002 6.18173L5.68174 8L7.50002 9.81827Z"
+                    fill="#4F4F4F"
+                  />
+                  <path
+                    d="M7.50002 9.81827L9.31829 8L7.50002 6.18173L5.68174 8L7.50002 9.81827Z"
+                    fill="#4F4F4F"
+                  />
                 </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </>
   )
 }

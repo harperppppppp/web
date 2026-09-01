bc012afab18d63512e634b0a83065ee21779c809
harperppppppp <harperp.bz@gmail.com>
2026-09-01 09:34:38 +0900

    ch09

---
+++ ch09/src/components/html/Input.tsx
@@ -0,0 +1,8 @@
+type InputProps = Omit<React.ComponentPropsWithRef<'input'>, 'type'> & {
+  type?: 'text';
+}
+
+export default function Input(props: InputProps) {
+  const { ...rest } = props;
+  return <input {...rest} />;
+}
